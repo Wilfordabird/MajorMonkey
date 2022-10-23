f83f27cdb52e7d51ffa295b98cd109a020febd1b
Gabriel Dos Santos <gabrieldossantos@Gabes-MacBook-Pro.local>
2022-10-23 14:53:55 -0400

    Stylistic updates

---
+++ constants.py
@@ -0,0 +1,18 @@
+"""Constants for Major Processing"""
+
+MAJORS = ['ACCT', 'USAF', 'AFAM', 'AFST', 'AKKD', 'ASL', 'AMST',
+'GREK', 'ANTH', 'AMTH', 'APHY', 'ARBC', 'ARCG', 'ARCH', 'ARMN',
+'ART', 'ASTR', 'BIOL', 'BENG', 'SBCR', 'BRST', 'BURM', 'CENG',
+'CHEM', 'CHLD', 'CHNS', 'CLCV', 'CLSS', 'CGSC', 'LITR', 'CSEC',
+'CPSC', 'CPAR', 'CZEC', 'DRST', 'DUTC', 'EPS', 'EALL', 'EAST',
+'E&EB', 'ECON', 'EDST', 'EGYP', 'EENG', 'ENRG', 'ENAS', 'ENGL',
+'ENVE', 'EVST', 'EP&E', 'ER&M', 'FILM', 'FNSH', 'F&ES', 'FREN',
+'GMAN', 'GLBL', 'HLTH', 'HEBR', 'HNDI', 'HIST', 'HSAR', 'HSHM',
+'HUMS', 'HMRT', 'HGRN', 'INDN', 'ITAL', 'JAPN', 'JDST', 'KHMR',
+'SWAH', 'KREN', 'LAST', 'LATN', 'LING', 'MATH', 'MENG', 'MGRK',
+'MMES', 'MTBT', 'MB&B', 'MCDB', 'MUSI', 'NAVY', 'NELC', 'NSCI',
+'OTTM', 'PERS', 'PHIL', 'PHYS', 'PLSH', 'PLSC', 'PORT', 'PSYC',
+'PNJB', 'RLST', 'ROMN', 'RSEE', 'RUSS', 'SKRT', 'SCIE', 'SNHL',
+'SLAV', 'SOCY', 'SAST', 'SPAN', 'SPEC', 'S&DS', 'STCY', 'TAML',
+'THST', 'DEVN', 'TBTN', 'TKSH', 'TWI', 'UKRN', 'URBN', 'VIET',
+'WLOF', 'WGSS', 'YORU', 'ZULU']
---
+++ csv_to_db.py
@@ -0,0 +1,1 @@
+"""Convert csv files into sql db"""
