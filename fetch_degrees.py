73614107c1bb8dcbce7d1114fe7bba6173f55ede
Gabriel Dos Santos <gabrieldossantos@Gabes-MBP.fios-router.home>
2022-11-26 11:47:07 -0500

    Maintain foreign key constraints

--- fetch_degrees.py
+++ fetch_degrees.py
@@ -5,24 +5,27 @@ import pandas as pd
 
 def main():
 
-    degrees_df = pd.read_csv("degrees.csv")
     requirements = pd.read_csv("requirements.csv")
-
-    for _, degree in degrees_df.iterrows():
-        degreename = degree[0]
-        requirements = degree[1]
-        electives = degree[2]
-        # degree_client = DegreeInsert(degreename, requirements, electives)
-        # degree_client.upload()
 
     # Loop through requirements
     for _, requirement in requirements.iterrows():
         requisitename = requirement[0]
         raw_requisite = requirement[1]
-        # Must processes raw_requisite into a structured list format
 
-        # requisite_client = RequisiteInsert(requisitename, clean_requisite)
-        # requisite_client.upload()
+        # TODO: Parse the requisite string into a list of lists!!!
+        # Must processes raw_requisite into a structured list format
+        clean_requisite = raw_requisite.split(";")
+
+        requisite_client = RequisiteInsert(requisitename, clean_requisite)
+        requisite_client.insert()
+
+    degrees_df = pd.read_csv("degrees.csv")
+    for _, degree in degrees_df.iterrows():
+        degreename = degree[0]
+        requirements = degree[1]
+        electives = degree[2]
+        degree_client = DegreeInsert(degreename, requirements, electives)
+        degree_client.insert()
 
 if __name__ == "__main__":
     main()
